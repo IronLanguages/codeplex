ae81a197f2c90f031dad8f9d789f193eed91cfb8
IPyTeam_cp <SND\IPyTeam_cp>
2006-11-02 21:14:08 +0000

    (135887)

--- IronPython_Main/Src/Tests/test_nt.py
+++ IronPython_Main/Src/Tests/test_nt.py
@@ -14,6 +14,7 @@
 ######################################################################################
 
 from lib.assert_util import *
+from lib.file_util import *
 
 import nt
 
@@ -59,8 +60,7 @@ def test_environ():
         AreEqual(System.Environment.GetEnvironmentVariable(non_exist_key), None)
     
     AreEqual(type(nt.environ), type({}))
-   
-test_environ()
+    
 
 AssertError(WindowsError, nt.startfile, "not_exist_file.txt")
 
@@ -87,20 +87,32 @@ AssertError(OSError, lambda:nt.chdir('tsd'))
 ################################################################################################
 # popen/popen2/popen3 tests
 
-# open a pipe just for reading...
-x = nt.popen('ping 127.0.0.1', 'r')
-text = x.read()
-Assert(text.lower().index('pinging') != -1)
-AreEqual(x.close(), None)
+def test_popen():
+    # open a pipe just for reading...
+    pipe_modes = [["ping 127.0.0.1", "r"],
+                  ["ping 127.0.0.1"]]
+    if is_cli:
+        pipe_modes.append(["ping 127.0.0.1", ""])
+        
+    for args in pipe_modes:
+        x = nt.popen(*args)
+        text = x.read()
+        Assert(text.lower().index('pinging') != -1)
+        AreEqual(x.close(), None)
 
-# bug 1146
-#x = nt.popen('sort', 'w')
-#x.write('hello\nabc\n')
-#AreEqual(x.close(), None)
+    #write to a pipe
+    x = nt.popen('sort', 'w')
+    x.write('hello\nabc\n')
+    x.close()
 
+    # bug 1146
+    #x = nt.popen('sort', 'w')
+    #x.write('hello\nabc\n')
+    #AreEqual(x.close(), None)
 
-# once w/ default mode
-# once w/ no mode specified
+    # once w/ default mode
+    AssertError(ValueError, nt.popen, "ping 127.0.0.1", "a")
+
 
 # once w/ no mode
 stdin, stdout = nt.popen2('sort')
@@ -195,5 +207,149 @@ def test_utime():
     y = nt.stat('temp_file_does_not_exist.txt')
     AreEqual(x[7], y[7])
     AreEqual(x[8], y[8])
+
+def test_tempnam():
+    '''
+    '''
+    #sanity checks
+    AreEqual(type(nt.tempnam()), str)
+    AreEqual(type(nt.tempnam("garbage name should still work")), str)
+    #BUG - "" does not work
+    #AreEqual(type(nt.tempnam("", "pre")), str)
     
-test_utime()
+    #Very basic case
+    joe = nt.tempnam()
+    last_dir = joe.rfind("\\")
+    temp_dir = joe[:last_dir+1]
+    Assert(directory_exists(temp_dir))
+    Assert(not file_exists(joe))
+    
+    #Basic case where we give it an existing directory and ensure
+    #it uses that directory
+    joe = nt.tempnam(get_temp_dir())
+    last_dir = joe.rfind("\\")
+    temp_dir = joe[:last_dir+1]
+    Assert(directory_exists(temp_dir))
+    Assert(not file_exists(joe))
+    AreEqual(nt.stat(temp_dir.strip("\\")), nt.stat(get_temp_dir()))
+    
+    #few random prefixes
+    prefix_names = ["", "a", "1", "_", ".", "sillyprefix", 
+                    "                                ", 
+                    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+                    ]
+    #test a few directory names that shouldn't really work
+    dir_names = ["b", "2", "_", ".", "anotherprefix", 
+                 "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+                 None]
+    
+    for dir_name in dir_names:
+        #just try the directory name on it's own
+        joe = nt.tempnam(dir_name)
+        last_dir = joe.rfind("\\")
+        temp_dir = joe[:last_dir+1]
+        Assert(directory_exists(temp_dir))
+        Assert(not file_exists(joe))
+        Assert(temp_dir != dir_name)
+            
+        #now try every prefix
+        for prefix_name in prefix_names:
+            joe = nt.tempnam(dir_name, prefix_name)
+            last_dir = joe.rfind("\\")
+            temp_dir = joe[:last_dir+1]
+            file_name = joe[last_dir+1:]
+            Assert(directory_exists(temp_dir))
+            Assert(not file_exists(joe))
+            Assert(temp_dir != dir_name)
+            Assert(file_name.startswith(prefix_name))
+        
+def test_times():
+    '''
+    '''
+    #simple sanity check
+    utime, stime, zero1, zero2, zero3 = nt.times()
+    Assert(utime>0)
+    Assert(stime>0)
+    AreEqual(zero1, 0)
+    AreEqual(zero2, 0)
+    #BUG - according to the specs this should be 0 for Windows
+    #AreEqual(zero3, 0)
+    
+    
+def test_putenv():
+    '''
+    '''
+    #simple sanity check
+    nt.putenv("IPY_TEST_ENV_VAR", "xyz")
+       
+    #ensure it really does what it claims to do
+    #TODO
+    
+    #negative cases
+    AssertError(TypeError, nt.putenv, None, "xyz")
+    #BUG
+    #AssertError(TypeError, nt.putenv, "ABC", None)
+    AssertError(TypeError, nt.putenv, 1, "xyz")
+    AssertError(TypeError, nt.putenv, "ABC", 1)
+    
+def test_spawnle():
+    '''
+    '''
+    #BUG?
+    #CPython nt has no spawnle function
+    if is_cli == False:
+        return
+    
+    ping_cmd = nt.environ["windir"] + "\system32\ping" 
+    
+    #simple sanity check
+    nt.spawnle(nt.P_WAIT, ping_cmd , "ping", "/?", {})
+    #BUG - the first parameter of spawnle should be "ping"
+    #nt.spawnle(nt.P_WAIT, ping_cmd , "ping", "127.0.0.1", {})
+    #BUG - even taking "ping" out, multiple args do not work
+    #pid = nt.spawnle(nt.P_NOWAIT, ping_cmd ,  "-n", "15", "-w", "1000", "127.0.0.1", {})
+    
+    #negative cases
+    AssertError(TypeError, nt.spawnle, nt.P_WAIT, ping_cmd , "ping", "/?", None)
+    AssertError(TypeError, nt.spawnle, nt.P_WAIT, ping_cmd , "ping", "/?", {1: "xyz"})
+    AssertError(TypeError, nt.spawnle, nt.P_WAIT, ping_cmd , "ping", "/?", {"abc": 1})
+    
+def test_tmpfile():
+    '''
+    '''
+    #sanity check
+    joe = nt.tmpfile()
+    AreEqual(type(joe), file)
+    joe.close()
+    
+def test_waitpid():
+    '''
+    '''
+    #sanity check    
+    #the usage of spawnle is a bug in this case that should be fixed in IP, 
+    #but since this test is for waitpid it's basically OK
+    #ping_cmd = nt.environ["windir"] + "\system32\ping" 
+    #pid = nt.spawnle(nt.P_NOWAIT, ping_cmd ,  "-n", "5", "-w", "1000", "127.0.0.1", {})
+    #new_pid, exit_stat = nt.waitpid(pid, 0)
+    
+    #negative cases
+    #BUG - should be an OSError instead of a ValueError
+    #AssertError(OSError, nt.waitpid, -1234, 0)
+    AssertError(TypeError, nt.waitpid, "", 0)
+    
+def test_spawnve():
+    '''
+    '''
+    ping_cmd = nt.environ["windir"] + "\system32\ping" 
+    
+    #simple sanity checks
+    nt.spawnve(nt.P_WAIT, ping_cmd, ["ping", "/?"], {})
+    nt.spawnve(nt.P_WAIT, ping_cmd, ["ping", "127.0.0.1"], {})
+    nt.spawnve(nt.P_WAIT, ping_cmd, ["ping", "-n", "15", "-w", "1000", "127.0.0.1"], {})
+    
+    #negative cases
+    AssertError(TypeError, nt.spawnve, nt.P_WAIT, ping_cmd , ["ping", "/?"], None)
+    AssertError(TypeError, nt.spawnve, nt.P_WAIT, ping_cmd , ["ping", "/?"], {1: "xyz"})
+    AssertError(TypeError, nt.spawnve, nt.P_WAIT, ping_cmd , ["ping", "/?"], {"abc": 1})
+    
+run_test(__name__)
