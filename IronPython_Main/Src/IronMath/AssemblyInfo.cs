a0ccd770d7a19493c73ec17f6fca7a2e79de5d0f
IPyTeam_cp <SND\IPyTeam_cp>
2007-03-21 16:22:57 +0000

    (186661)

--- IronPython_Main/Src/IronMath/AssemblyInfo.cs
+++ IronPython_Main/Src/IronMath/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Security.Permissions;
 // associated with an assembly.
 //
 [assembly: AssemblyTitle("IronMath")]
-[assembly: AssemblyDescription("IronPython Math Assembly 1.1b1")]
+[assembly: AssemblyDescription("IronPython Math Assembly 1.1")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("IronPython")]
