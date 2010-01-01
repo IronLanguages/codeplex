ee41cbcc75cfac3281f4935ba133eeebe3a77d66
IPyTeam_cp <SND\IPyTeam_cp>
2010-01-01 16:52:47 +0000

    -------------------------------------------------------------------------------- Changeset Id: 1407692 Date: 12/31/2009 2:08:00 PM
    - test_memory.py: IP is now gobbling up more memory seemingly randomly
    
    
    (Shelveset: MISC;REDMOND\dfugate | SNAP CheckinId: 10070)
    --------------------------------------------------------------------------------
    Changeset Id: 1407651
    Date: 12/31/2009 12:14:04 PM
    
    - socket_test.py: commented out a test previously thought to be a negative scenario which in fact passes on several Win7 and Win2008 boxes.
    
    
    (Shelveset: SOCKET_RELIABLE;REDMOND\dfugate | SNAP CheckinId: 10069)

--- Src/Runtime/Microsoft.Scripting/Properties/AssemblyInfo.cs
+++ Src/Runtime/Microsoft.Scripting/Properties/AssemblyInfo.cs
@@ -63,7 +63,7 @@ using System.Security;
 
 #if !SILVERLIGHT
 [assembly: AssemblyVersion("0.9.6.20")]
-[assembly: AssemblyFileVersion("2.6.0912.0")]
+[assembly: AssemblyFileVersion("2.6.1001.0")]
 [assembly: AssemblyInformationalVersion("2.6")]
 [assembly: AllowPartiallyTrustedCallers]
 #endif
