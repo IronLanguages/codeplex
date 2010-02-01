0714ccbcd4c1e1bf7e9c38f5c1def1f10059aa64
IPyTeam_cp <SND\IPyTeam_cp>
2010-02-01 16:53:50 +0000

    -------------------------------------------------------------------------------- Changeset Id: 1544225 Date: 1/29/2010 4:53:31 PM
    Added regression tests for a handful of 'dynamic' bugs filed internally.
    
    
    (Shelveset: DYN_REGRESSIONS;REDMOND\dfugate | SNAP CheckinId: 10237)

--- IronPython_Main/Src/Tests/interop/net/dynamic/dynamic_regressions.py
+++ IronPython_Main/Src/Tests/interop/net/dynamic/dynamic_regressions.py
@@ -52,6 +52,84 @@ def test_cp24117():
         AreEqual(DR.cp24117(xrange),    "IronPython.Runtime.Types.PythonType")
         AreEqual(DR.cp24117(xrange(3)), "IronPython.Runtime.XRange")
 
+#--CodePlex 24118--#
+def GetMethodTest():
+  return Method01()
+
+class Method01(object):
+  #Function 
+  def Normal01(self, a, b):
+    return a + b
+
+  #Function 
+  def Optional01(self, a, b=1):
+    return a + b
+
+@disabled("http://ironpython.codeplex.com/WorkItem/View.aspx?WorkItemId=26089")
+def test_cp24118():
+    #TODO: once 26089 gets fixed the following needs actual verification.  That is,
+    #right now DR.cp24118 just calls GetMethodTest without doing much validation.
+    DR.cp24118(sys.modules[__name__])
+
+def test_cp24115():
+    class TestObj(object): pass
+    DR.cp24115(TestObj())
+
+def test_cp24111():
+    class TestObj(object):
+        def __init__(self, nz):
+            self.nz = nz
+        def __nonzero__(self):
+            return self.nz
+
+    for x in [0, 1]:
+        AreEqual(DR.cp24111(TestObj(x)), not TestObj(x))
+
+def test_cp24088():
+    from IronPythonTest import DelegateTest
+    AssertErrorWithMessage(Exception, "Operator '+=' cannot be applied to operands of type 'IronPython.Runtime.Types.ReflectedEvent.BoundEvent' and 'int'",
+                           DR.cp24088, DelegateTest.Event)
+
+cp24113_vb_snippet = '''
+Imports System.Dynamic
+
+Public Module CodePlex24113
+
+    Public Function cp24113(ByVal test As Object)
+        test("hello") = "hi"
+        Return test("abc")
+    End Function
+
+End Module
+'''
+
+def test_cp24113():
+    import os
+    from iptest.process_util import run_vbc
+    from iptest.assert_util import  testpath
+
+    cp24113_vb_filename = testpath.temporary_dir + r"\cp24113_vb_module.vb"
+    f = open(cp24113_vb_filename, "w")
+    f.writelines(cp24113_vb_snippet)
+    f.close()
+
+    cp24113_vb_dllname  = testpath.temporary_dir + r"\cp24113_vb_dll"
+    run_vbc("/target:library /out:%s %s" % (cp24113_vb_dllname, cp24113_vb_filename))
+    clr.AddReferenceToFileAndPath(cp24113_vb_dllname)
+    import CodePlex24113
+
+    class TestObj(object):
+        Prop = None
+        def __getitem__(self, key): 
+            return key
+        def __setitem__(self, key, item): 
+            self.Prop = item
+
+    to = TestObj()
+    AreEqual(to.Prop, None)
+    AreEqual(CodePlex24113.cp24113(to), "abc")
+    AreEqual(to.Prop, "hi")
+
 #------------------------------------------------------------------------------
 #--Main
 run_test(__name__)
